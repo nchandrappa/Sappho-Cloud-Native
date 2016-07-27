c202a7b8ac5630da2b4b15aa8bc24ed4a6e0a5b4
nchandrappa <nchandrappa@pivotal.io>
2016-07-27 10:50:55 -0400

    first commit

---
+++ fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/services/fortunes/FortuneService.java
@@ -0,0 +1,24 @@
+package io.spring.cloud.samples.fortuneteller.ui.services.fortunes;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class FortuneService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallbackFortune")
+    public Fortune randomFortune() {
+        return restTemplate.getForObject("http://fortunes/random", Fortune.class);
+    }
+
+    @SuppressWarnings("unused")
+	private Fortune fallbackFortune() {
+        return new Fortune(42L, "Your future is unclear.");
+    }
+}
